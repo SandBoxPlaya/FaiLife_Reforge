2681c7a21f5ef36b5245a5f9168d611b2473f037
SandBoxPlaya <147025255+SandBoxPlaya@users.noreply.github.com>
2023-11-20 14:37:17 +0200

    slider changes

--- FaiLife/Assets/Scripts/MusicScripts/MusicScript.cs
+++ FaiLife/Assets/Scripts/MusicScripts/MusicScript.cs
@@ -10,11 +10,16 @@ public class Music : MonoBehaviour
     [SerializeField] public AudioSource audioLine1;
     [SerializeField] public AudioSource audioLine2;
     [SerializeField] public Slider musicSlider;
+    private Transform handleTransform;
 
     private void Start()
     {
         audioLine1.loop = true;
         audioLine2.loop = true;
+
+        float volume = float.Parse(PlayerPrefs.GetString("MasterVolume", "0.5"));
+        audioLine1.volume = volume;
+        musicSlider.normalizedValue = volume;
     }
 
     public void SetMusicVolume()
@@ -24,5 +29,10 @@ public class Music : MonoBehaviour
 
         float volume2 = musicSlider.value;
         audioLine2.volume = volume2;
+    }
+
+    public void SaveMasterVolumeSettings()
+    {
+        PlayerPrefs.SetString("MasterVolume", audioLine1.volume.ToString());
     }
 }
